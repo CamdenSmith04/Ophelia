d91755fb12cf849bbbca9ecf56685f6e9bd4ac16
ariasjuliana <ariasjulianar@gmail.com>
2025-02-21 20:49:45 -0500

    h

--- Juli/basicScript.py
+++ Juli/basicScript.py
@@ -7,3 +7,6 @@ data = yf.download(ticker,start='2020-01-01',end='2025-01-01',interval='1d')
 print(data['Low'])
 
 
+import robin_stocks.robinhood as rh
+
+g
--- Juli/methods.py
+++ Juli/methods.py
@@ -8,7 +8,6 @@ printHello('Camden')
 def addIntegers(num1, num2, num3):
     print(num1+num2+num3)
 
-
 addIntegers(5,10,15)
 
 
