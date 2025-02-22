028b9acfc19a313b53fceb4081691735fb92e879
CamdenSmith04 <ctsmith0319@gmail.com>
2025-02-21 20:46:20 -0500

    Files

---
+++ Camden/basicScript.py
@@ -0,0 +1,7 @@
+import yfinance as yf
+import pandas as pd
+
+ticker = "MSFT"
+data = yf.download(ticker, start="2020-01-01",end="2025-01-01",interval="1d")
+
+print(data["Low"])
---
+++ Camden/methods.py
@@ -0,0 +1,10 @@
+def printHello(name):
+    print(f"Hello {name}!")
+
+
+def addIntegers(num1, num2, num3):
+    print(num1+num2+num3)
+
+
+addIntegers(5,10,15)
+addIntegers(2,4,6)
