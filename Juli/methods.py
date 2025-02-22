c0b3cbc7d080048a2422647290875ba5dece1d15
ariasjuliana <ariasjulianar@gmail.com>
2025-02-21 20:47:47 -0500

    o

---
+++ Juli/methods.py
@@ -0,0 +1,15 @@
+def printHello(name):
+    print(f'Hello {name}!')
+
+
+printHello('John')
+printHello('Camden')
+
+def addIntegers(num1, num2, num3):
+    print(num1+num2+num3)
+
+
+addIntegers(5,10,15)
+
+
+
